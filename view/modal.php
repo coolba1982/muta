ec2b54af466d803100080d36fd15b52d044754a2
cmm <cmm>
2017-03-12 11:46:12 +0800

    dddddddddd

--- view/modal.php
+++
@@ -1,8 +0,0 @@
-<div class="modal hide fade"  id='md'>
-
-    <div class="modal-body" style="text-align: center;">
-
-        <img  src='res/img/loading.jpg' style='height:20px;width:20px;'/> <span id='load_info'></span>
-    </div>
-
-</div>
